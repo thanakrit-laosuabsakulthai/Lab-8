8d9b8933bcc4a9d1b83fb63795497a5f66b51289
Thanakrit <thanakrit_la@cmu.ac.th>
2025-12-16 15:31:05 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,58 @@
 "Brook"
 "Jinbe"
 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string getCharacter() {
+	int age, height, bounty;
+	int one_hundred_million = 100000000;
+
+	cout << "Enter your age: ";
+	cin >> age;
+
+	if(age <= 25) {
+		cout << "Enter your height: ";
+		cin >> height;
+		
+		if(height < 100) {
+			return "Chopper";
+		} else if (height < 180) {
+			return "Usopp";
+		} else {
+			cout << "Enter your bounty: ";
+			cin >> bounty;
+
+			if(bounty > 11 * one_hundred_million) {
+				return "Zoro";
+			} else {
+				return "Sanji";
+			}
+		}
+	}
+
+	if(age <= 60) {
+		cout << "Enter your bounty: ";
+		cin >> bounty;
+
+		if(bounty > 5 * one_hundred_million) {
+			return "Jinbe";
+		} else {
+			return "Franky";
+		}
+	} else {
+		return "Brook";
+	}
+}
+
+
+
+int main()
+{
+	string character = getCharacter();
+	cout << "Your character = " << character << endl;
+	return 0;
+}
